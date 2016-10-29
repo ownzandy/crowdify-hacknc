f7bbe2d56c58af2c1600fb2fbb91355ca291f5b2
Briguy52 <Briguy52@gmail.com>
2016-10-29 14:02:45 -0400

    commit before trying range slider

--- Crowdify/EditorViewController.swift
+++ Crowdify/EditorViewController.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+class EditorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
