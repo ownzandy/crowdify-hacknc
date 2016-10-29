f266a2ab83f78962c917fe3336c4c70dd18ffa64
Briguy52 <Briguy52@gmail.com>
2016-10-29 17:59:35 -0400

    added logic to convert MS to min:sec

--- Crowdify/EditorViewController.swift
+++ Crowdify/EditorViewController.swift
@@ -16,74 +16,43 @@ let endLabel = UILabel(frame: CGRect.zero)//init(origin: CGPoint.init(x: 20, y:2
 
 class EditorViewController: UIViewController {
     let rangeSlider = MARKRangeSlider(frame: CGRect.zero)
-    var songDurationMS = 10000 // TODO: take in as argument
-
-    /*
-    let rangeSlider = RangeSlider(frame: CGRect.zero)
+    var songDurationMS = 400000 // TODO: take in as argument
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Individual slider
-        view.addSubview(rangeSlider)
-        view.addSubview(startLabel)
-        view.addSubview(endLabel)
-        
-        startLabel.text = "0"
-        endLabel.text = "100"
-        /*
-        startLabel.snp.makeConstraints { make in
-            make.width.height.equalTo(100)
-            make.centerX.equalTo(rangeSlider).offset(rangeSlider.lowerValue)
-            make.centerY.equalTo(rangeSlider).offset(-15)
-        }
-        
-        endLabel.snp.makeConstraints { make in
-            make.width.height.equalTo(100)
-            make.centerX.equalTo(rangeSlider).offset(50)
-            make.centerY.equalTo(rangeSlider).offset(-15)
-        }
-        */
-        rangeSlider.addTarget(self, action: "rangeSliderValueChanged:", for: .valueChanged)
-        /*
-        //let time = DispatchTime.now().rawValue//dispatch_time(dispatch_time_t(DispatchTime.now().rawValue), Int64(NSEC_PER_SEC))
-        //ispatch_after(time, DispatchQueue.main, {//dispatch_get_main_queue()) {
-        DispatchQueue.main.async {
-            self.rangeSlider.trackHighlightTintColor = UIColor.purple
-            self.rangeSlider.curvaceousness = 1.0
-            //self.startLabel.center.x = CGFloat(self.rangeSlider.lowerValue)
-        }
- */
-    }
- 
-    */
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        // Config rangeSlider
         rangeSlider.addTarget(self, action: #selector(self.rangeSliderValueDidChange), for: .valueChanged)
         rangeSlider.setMinValue(0.0, maxValue: 1.0)
         rangeSlider.setLeftValue(0.2, rightValue: 0.7)
-        rangeSlider.minimumDistance = 0.2
+        rangeSlider.minimumDistance = 0.0
         view.addSubview(self.rangeSlider)
         
-        startLabel.text = rangeSlider.leftValue.description
-        endLabel.text = rangeSlider.rightValue.description
-        
+        startLabel.text = self.percentToTime(percentage: rangeSlider.leftValue, durationMS: self.songDurationMS)
+        endLabel.text = self.percentToTime(percentage: rangeSlider.rightValue, durationMS: self.songDurationMS)
         view.addSubview(startLabel)
         view.addSubview(endLabel)
         
         startLabel.snp.makeConstraints { make in
             make.width.height.equalTo(100)
-            make.centerX.equalTo(rangeSlider.leftThumbView)
-            make.centerY.equalTo(rangeSlider).offset(-20)
+            make.centerX.equalTo(rangeSlider.leftThumbView).offset(30)
+            make.centerY.equalTo(rangeSlider).offset(-30)
         }
         
         endLabel.snp.makeConstraints { make in
             make.width.height.equalTo(100)
-            make.centerX.equalTo(rangeSlider.rightThumbView)
+            make.centerX.equalTo(rangeSlider.rightThumbView).offset(30)
             make.centerY.equalTo(rangeSlider).offset(30)
         }
- 
+    }
+    
+    func percentToTime(percentage: CGFloat, durationMS: Int) -> String {
+        let totalMS = Int(Float(percentage) * Float(durationMS))
+        let timeSec = totalMS / 1000
+        let mins = timeSec / 60
+        let secs = timeSec % 60
+        let retval = String(mins) + ":" + ((secs<10) ? "0" : "") + String(secs)
+        return retval
     }
     
     override func viewDidLayoutSubviews() {
@@ -94,11 +63,7 @@ class EditorViewController: UIViewController {
     }
     
     func rangeSliderValueDidChange(_ slider: MARKRangeSlider) {
-        print(String(format: "%0.2f - %0.2f", slider.leftValue, slider.rightValue))
-    
-        //startLabel.center.x = CGFloat(slider)
-        //startLabel.center.x = CGFloat(slider.leftValue * 100)
-        startLabel.text = slider.leftValue.description
-        endLabel.text = slider.rightValue.description
+        startLabel.text = self.percentToTime(percentage: slider.leftValue, durationMS: self.songDurationMS)
+        endLabel.text = self.percentToTime(percentage: slider.rightValue, durationMS: self.songDurationMS)
     }
 }
