87ed84b1e97608711156f8490b31657d4f414b24
Briguy52 <Briguy52@gmail.com>
2016-10-29 16:36:17 -0400

    trying to place slider labels

--- Crowdify/EditorViewController.swift
+++ Crowdify/EditorViewController.swift
@@ -10,13 +10,37 @@ import Foundation
 import UIKit
 import SnapKit
 
+let startLabel = UILabel(frame: CGRect.zero)
+let endLabel = UILabel(frame: CGRect.init(origin: CGPoint.init(x: 20, y:20), size: CGSize.init(width: 10, height: 10)))
+
 class EditorViewController: UIViewController {
     let rangeSlider = RangeSlider(frame: CGRect.zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Individual slider
         view.addSubview(rangeSlider)
+        view.addSubview(startLabel)
+        view.addSubview(endLabel)
+        
+        startLabel.text = "0"
+        endLabel.text = "100"
+        
+        startLabel.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+            //make.centerX.centerY.equalTo(view)
+            //make.centerX.equalTo(view)
+            make.centerX.equalTo(rangeSlider).offset(rangeSlider.lowerValue)
+            make.centerY.equalTo(rangeSlider).offset(-15)
+        }
+        
+        endLabel.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+            //make.centerX.centerY.equalTo(view)
+            make.centerX.equalTo(rangeSlider).offset(-50)
+            make.centerY.equalTo(rangeSlider).offset(-15)
+        }
         
         rangeSlider.addTarget(self, action: Selector("rangeSliderValueChanged:"), for: .valueChanged)
         
@@ -25,6 +49,7 @@ class EditorViewController: UIViewController {
         DispatchQueue.main.async {
             self.rangeSlider.trackHighlightTintColor = UIColor.purple
             self.rangeSlider.curvaceousness = 1.0
+            //self.startLabel.center.x = CGFloat(self.rangeSlider.lowerValue)
         }
     }
     
@@ -37,5 +62,8 @@ class EditorViewController: UIViewController {
     
     func rangeSliderValueChanged(rangeSlider: RangeSlider) {
         print("Range slider value changed: (\(rangeSlider.lowerValue) \(rangeSlider.upperValue))")
+        
+        startLabel.center.x = CGFloat(rangeSlider.lowerValue)
+        startLabel.center.y = CGFloat(rangeSlider.upperValue)
     }
 }
