0f999fe3b6c09362c5084907964bdbd05230077e
Andy <ownz.andy@gmail.com>
2016-10-29 14:26:35 -0400

    adding search tracks from spotify

---
+++ Crowdify/Track.swift
@@ -0,0 +1,16 @@
+//
+//  Track.swift
+//  Crowdify
+//
+//  Created by Andy Wang on 10/29/16.
+//  Copyright © 2016 Andy Wang. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+    let name: String
+    let uri: URL
+    let artists: [String]
+    let coverArt: URL
+}
