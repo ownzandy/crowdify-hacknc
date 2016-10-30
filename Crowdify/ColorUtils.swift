5f16ba42c9844a088736322f329947e9d72b50ff
Briguy52 <Briguy52@gmail.com>
2016-10-30 04:57:04 -0400

    added button utils/styling

---
+++ Crowdify/ColorUtils.swift
@@ -0,0 +1,33 @@
+//
+//  ColorUtils.swift
+//  Crowdify
+//
+//  Created by Brian Lin on 10/30/16.
+//  Copyright © 2016 Andy Wang. All rights reserved.
+//
+
+import Foundation
+
+class ColorUtils {
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
